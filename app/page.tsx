a6d33cad8578b59d5cb258fa69278d0671650085
felipe-sbm <felipesbm2005@gmail.com>
2024-06-21 10:40:43 -0300

    Mudança de projeto

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { BlogPosts } from "app/components/posts";
-
 export default function Page() {
   return (
     <section>
       <div className="my-8">
-        <BlogPosts />
       </div>
     </section>
   );
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,11 @@
-import { getBlogPosts } from 'app/categorias/utils'
-
 export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/categorias/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }))
 
   let routes = ['', '/categorias'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes, ...blogs]
+  return [...routes]
 }
